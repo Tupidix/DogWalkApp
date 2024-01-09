55e638aa94924776693cf130312c433761fb5bb4
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2024-01-09 13:22:13 +0100

    navigation modifiée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {addIcons} from 'ionicons'
+import {navigateCircleOutline,  personOutline, pawOutline} from 'ionicons/icons'
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({navigateCircleOutline, personOutline, pawOutline})
+  }
 }
