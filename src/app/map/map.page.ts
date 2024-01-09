d8b253102ef0c0d1074c2cce3b5573a7b9d95cd7
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2024-01-09 16:31:43 +0100

    Introduction à la page map walkers... + tentative mise de la carte

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -4,8 +4,19 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonSegment,
+  IonLabel,
+  IonSegmentButton,
+  IonItem,
+  IonListHeader,
+  IonList,
+  IonButton,
 } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+// Carte
+import { AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
@@ -18,8 +29,29 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
     IonTitle,
     IonContent,
     ExploreContainerComponent,
+    IonSegment,
+    IonLabel,
+    IonSegmentButton,
+    IonItem,
+    IonListHeader,
+    IonList,
+    IonButton,
   ],
 })
+// export class MapPage implements AfterViewInit {
 export class MapPage {
   constructor() {}
-}
+
+  // ngAfterViewInit() {
+  //   this.initMap();
+  // }
+
+  // initMap() {
+  //   const map = L.map('map').setView([46.7813058, 6.6473608], 13);
+
+  //   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
+  //     map
+  //   );
+
+  // Ajoute des marqueurs, lignes, ou autres éléments à la carte si nécessaire
+} //}
