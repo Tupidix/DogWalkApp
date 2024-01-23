8a4d893489903590f4f594c51d00135e3f20ede5
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2024-01-23 15:38:39 +0100

    Affichage des informations de l'API dans walkers sans pour autant avoir fait le tri avec la géoloc

--- src/app/maps/walkers/walkers.page.ts
+++ src/app/maps/walkers/walkers.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/security/auth.service';
+import { shuffle } from 'ionicons/icons';
 
 @Component({
   selector: 'app-walkers',
@@ -13,6 +14,8 @@ import { AuthService } from 'src/app/security/auth.service';
   imports: [IonicModule, CommonModule, FormsModule],
 })
 export class WalkersPage implements OnInit {
+  allUsers: any[] = [];
+
   constructor(
     // Inject the authentication provider.
     private auth: AuthService,
@@ -25,10 +28,14 @@ export class WalkersPage implements OnInit {
     //   console.log('token: ' + token);
     // });
     this.auth.getAllUsers$().subscribe((users) => {
-      let allUsers = users;
-      allUsers.forEach((user) => {
-        console.log(user.firstname);
-      });
+      this.allUsers = users;
+
+      // Temporaire, prendre les 5 premiers résultats
+      this.allUsers = this.allUsers.slice(0, 5);
+
+      // this.allUsers.forEach((user) => {
+      //   console.log(user);
+      // });
     });
   }
 }
