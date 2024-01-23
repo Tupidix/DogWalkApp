8c039501f48ef5462809e176e0c0be71253e463f
quinteirosm <quinteiros.m@gmail.com>
2024-01-23 15:04:50 +0100

    avance dogs

--- src/app/dogs/dogs.page.ts
+++ src/app/dogs/dogs.page.ts
@@ -2,19 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/security/auth.service';
 
 @Component({
   selector: 'app-dogs',
   templateUrl: './dogs.page.html',
   styleUrls: ['./dogs.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class DogsPage implements OnInit {
-
-  constructor() { }
+  constructor(
+    // Inject the authentication provider.
+    private auth: AuthService,
+    // Inject the router
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.auth.getAllDogs$().subscribe((dogs) => {
+      localStorage.setItem('dogs', JSON.stringify(dogs));
+      console.dir(dogs);
+    });
   }
-
 }
