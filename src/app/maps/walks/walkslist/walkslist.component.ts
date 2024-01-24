bf15c23a08256554666392e124c6a70789e24f41
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2024-01-24 16:44:36 +0100

    Small things

--- src/app/maps/walks/walkslist/walkslist.component.ts
+++ src/app/maps/walks/walkslist/walkslist.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AuthService } from 'src/app/security/auth.service';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-walkslist',
