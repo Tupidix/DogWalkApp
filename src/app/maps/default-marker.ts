65d41035c0817bafafdb2e66dd75bf75dcb99298
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2024-01-24 11:40:36 +0100

    Rajout du pin sur la carte

--- src/app/maps/default-marker.ts
+++ src/app/maps/default-marker.ts
@@ -6,9 +6,10 @@ export const defaultIcon: Icon<IconOptions> = icon({
   // This defines the pixel that should be placed right above the location
   // If not provided, the image center will be used, and that could be awkward
   iconAnchor: [13, 41],
-  // The path to the image to display. In this case, it's a Leaflet asset
-  // i research pin.png in my project, is in the assets folder
-  iconUrl: 'assets/pin.png',
+  // The path to the image to display
+  // i research pin.png in my project
+  iconUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Maps_pin.svg',
   // The path to the image's shadow to display. Also a leaflet asset
   //   shadowUrl: 'leaflet/marker-shadow.png',
 });
