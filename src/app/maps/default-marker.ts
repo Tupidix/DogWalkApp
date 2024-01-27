5094c5f45589652938729bc7fea94f8e118b3ea0
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2024-01-27 12:25:43 +0100

    Alignement des boutons et de la carte

--- src/app/maps/default-marker.ts
+++ src/app/maps/default-marker.ts
@@ -7,7 +7,6 @@ export const defaultIcon: Icon<IconOptions> = icon({
   // If not provided, the image center will be used, and that could be awkward
   iconAnchor: [13, 41],
   // The path to the image to display
-  // i research pin.png in my project
   iconUrl:
     'https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Maps_pin.svg',
   // The path to the image's shadow to display. Also a leaflet asset
@@ -16,12 +15,11 @@ export const defaultIcon: Icon<IconOptions> = icon({
 
 export const MyLocationIcon: Icon<IconOptions> = icon({
   // This define the displayed icon size, in pixel
-  iconSize: [25, 41],
+  iconSize: [25, 25],
   // This defines the pixel that should be placed right above the location
   // If not provided, the image center will be used, and that could be awkward
   iconAnchor: [13, 41],
   // The path to the image to display
-  // i research pin.png in my project
   iconUrl:
     'https://upload.wikimedia.org/wikipedia/commons/8/83/Simple_icon_location.svg',
   // The path to the image's shadow to display. Also a leaflet asset
