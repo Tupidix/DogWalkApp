3d1c6190ccea1a2144dd9b86cb56f84ffa612d59
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2024-01-27 22:14:32 +0100

    Tracking sur carte, mesure en temps réel, remise au centre, manque bouton stop recording

--- src/app/createawalk/createawalk.page.ts
+++ src/app/createawalk/createawalk.page.ts
@@ -4,9 +4,17 @@ import { FormsModule, NonNullableFormBuilder } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 // Carte
-import { latLng, MapOptions, tileLayer, Map, marker, Marker } from 'leaflet';
+import {
+  latLng,
+  MapOptions,
+  tileLayer,
+  Map,
+  marker,
+  Marker,
+  LatLngExpression,
+} from 'leaflet';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { MyLocationIcon } from '../maps/default-marker';
+import { MyLocationIcon, trackingIcon } from '../maps/default-marker';
 
 // Geolocalisation
 import { Geolocation } from '@capacitor/geolocation';
@@ -19,6 +27,12 @@ import { Geolocation } from '@capacitor/geolocation';
   imports: [IonicModule, CommonModule, FormsModule, LeafletModule],
 })
 export class CreateawalkPage implements OnInit {
+  positions: Array<{ lat: number; lng: number }> = [];
+  watchIds: any[] = [];
+
+  distanceTraveled: number = 0;
+  positionsPourDistance: Array<{ lat: number; lng: number }> = [];
+
   heure: number | string;
   minute: number | string;
 
@@ -27,6 +41,10 @@ export class CreateawalkPage implements OnInit {
 
   mapMarkers: Marker[] = [];
   isModalOpen = false;
+
+  noPoint: number = 1;
+
+  currentPosition: any = {};
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
@@ -41,7 +59,7 @@ export class CreateawalkPage implements OnInit {
     this.heure = this.heure < 10 ? '0' + this.heure : this.heure;
     this.minute = this.minute < 10 ? '0' + this.minute : this.minute;
 
-    this.getUserLocation();
+    // this.getUserLocation();
 
     this.mapOptions = {
       layers: [
@@ -67,24 +85,135 @@ export class CreateawalkPage implements OnInit {
     setTimeout(() => map.invalidateSize(), 100);
   }
 
-  getUserLocation() {
-    Geolocation.getCurrentPosition()
-      .then((position) => {
-        const userLocation = {
+  ngOnInit() {
+    this.watchPosition();
+  }
+
+  // La formule de Haversine permet de calculer la distance entre deux points sur une sphère, connaissant leurs coordonnées géographiques.
+  haversine_distance(mk1: any, mk2: any) {
+    // console.log('1');
+    let R = 6371.071; // Radius of the Earth in Kilometers
+    // console.log('2');
+    let rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
+    // console.log('3');
+    let rlat2 = mk2.lat * (Math.PI / 180); // Convert degrees to radians
+    // console.log('4');
+    let difflat = rlat2 - rlat1; // Radian difference (latitudes)
+    // console.log('5');
+    let difflon = (mk2.lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+    // console.log('6');
+    let d =
+      2 *
+      R *
+      Math.asin(
+        Math.sqrt(
+          Math.pow(Math.sin(difflat / 2), 2) +
+            Math.cos(rlat1) *
+              Math.cos(rlat2) *
+              Math.pow(Math.sin(difflon / 2), 2)
+        )
+      );
+
+    return d * 1000; // Mètres
+  }
+
+  watchPosition(): void {
+    Geolocation.watchPosition({ enableHighAccuracy: true }, (position) => {
+      console.log('watchPosition');
+      if (position) {
+        this.currentPosition = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        this.map?.setView(userLocation, 14); // 13 est le niveau de zoom
-        this.mapMarkers.push(
-          marker([userLocation.lat, userLocation.lng], {
-            icon: MyLocationIcon,
-          }).bindTooltip('Your position')
-        );
-      })
-      .catch((err) => {
-        console.error('Error getting user location', err);
-      });
+
+        /*
+        TOUTE LA PARTIE CALCUL DE DISTANCE
+        */
+        if (this.positionsPourDistance.length == 0) {
+          this.positionsPourDistance.push(this.currentPosition);
+        } else if (
+          this.positionsPourDistance[this.positionsPourDistance.length - 1]
+            .lat !== this.currentPosition.lat ||
+          this.positionsPourDistance[this.positionsPourDistance.length - 1]
+            .lng !== this.currentPosition.lng
+        ) {
+          this.distanceTraveled =
+            (Math.round(
+              this.distanceTraveled +
+                this.haversine_distance(
+                  this.positionsPourDistance[
+                    this.positionsPourDistance.length - 1
+                  ],
+                  this.currentPosition
+                )
+            ) /
+              1000) *
+            1000;
+          this.positionsPourDistance.push(this.currentPosition);
+        }
+
+        console.log('this.positionsPourDistance');
+        console.log(this.positionsPourDistance);
+
+        /*
+        FIN DE LA PARTIE CALCUL DE DISTANCE */
+
+        // Si c'est la première position on centre la carte sur la position actuelle et$
+        // on ajoute un marqueur et on ajoute la position dans le tableau
+        //(on pourrait également le faire plus bas avec le if lastPosition)
+        if (this.positions.length == 0) {
+          this.map?.setView(this.currentPosition, 14); // 14 est le niveau de zoom
+          this.mapMarkers.push(
+            marker([this.currentPosition.lat, this.currentPosition.lng], {
+              icon: MyLocationIcon,
+            }).bindTooltip('Your beginning position')
+          );
+          this.positions.push(this.currentPosition);
+        } else {
+          // Lorsque la position change ajoute un marqueur
+          // ce n'est pas pour autant que l'on ajoute la position dans le tableau pour stocker dans la BD
+          // Mais on stocke dans le tableau qui sert à calculer la distance parcourue
+          this.mapMarkers.push(
+            marker([this.currentPosition.lat, this.currentPosition.lng], {
+              icon: trackingIcon,
+            }).bindTooltip(this.noPoint++ + ' from start position')
+          );
+
+          /*
+          Partie calcul distance
+          */
+
+          /*
+          Fin partie calcul distance
+          */
+
+          const lastPosition = this.positions[this.positions.length - 1];
+
+          // Si la dernière position est à plus de 30 mètres de la position actuelle alors on l'ajoute dans le tableau
+          if (
+            lastPosition &&
+            this.haversine_distance(lastPosition, this.currentPosition) >= 30
+          ) {
+            this.positions.push(this.currentPosition);
+            console.log('Position ajoutée car différence de minimum 30 mètres');
+          }
+        }
+      }
+    });
   }
 
-  ngOnInit() {}
+  centerMap() {
+    console.log('centerMap');
+    // Get the current position of the device
+
+    this.map?.setView(this.currentPosition, 14); // 13 est le niveau de zoom
+  }
+
+  stopWatching(): void {
+    console.log('stopWatching');
+    console.log("résumé de l'itinéraire");
+    console.log('Nombre de positions : ' + this.positions.length);
+    console.log(this.positions);
+    this.watchIds.forEach((watchId) => Geolocation.clearWatch({ id: watchId }));
+  }
 }
--- src/app/maps/default-marker.ts
+++ src/app/maps/default-marker.ts
@@ -5,7 +5,7 @@ export const defaultIcon: Icon<IconOptions> = icon({
   iconSize: [25, 41],
   // This defines the pixel that should be placed right above the location
   // If not provided, the image center will be used, and that could be awkward
-  iconAnchor: [13, 41],
+  iconAnchor: [13, 41], // Le bas du marqueur est placé sur la position
   // The path to the image to display
   iconUrl:
     'https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Maps_pin.svg',
@@ -15,13 +15,26 @@ export const defaultIcon: Icon<IconOptions> = icon({
 
 export const MyLocationIcon: Icon<IconOptions> = icon({
   // This define the displayed icon size, in pixel
-  iconSize: [25, 25],
+  iconSize: [20, 20],
   // This defines the pixel that should be placed right above the location
   // If not provided, the image center will be used, and that could be awkward
-  iconAnchor: [13, 41],
+  iconAnchor: [10, 10], // Centre de l'image est sur la position
   // The path to the image to display
   iconUrl:
     'https://upload.wikimedia.org/wikipedia/commons/8/83/Simple_icon_location.svg',
+  // The path to the image's shadow to display. Also a leaflet asset
+  //   shadowUrl: 'leaflet/marker-shadow.png',
+});
+
+export const trackingIcon: Icon<IconOptions> = icon({
+  // This define the displayed icon size, in pixel
+  iconSize: [10, 10],
+  // This defines the pixel that should be placed right above the location
+  // If not provided, the image center will be used, and that could be awkward
+  iconAnchor: [5, 5],
+  // The path to the image to display
+  iconUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/4/48/Light_Blue_Circle.svg',
   // The path to the image's shadow to display. Also a leaflet asset
   //   shadowUrl: 'leaflet/marker-shadow.png',
 });
