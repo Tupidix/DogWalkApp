4e7daf08cfb849350dbec8f4f17ad3541afc68ae
quinteirosm <quinteiros.m@gmail.com>
2024-01-28 22:21:15 +0100

    env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://dogwalkapi.onrender.com',
+  qimgUrl: 'https://comem-qimg.onrender.com/api',
+  qimgSecret:
+    'f0mFxxX+QnGPX6i+iLg8nB+v2wzcV9XMcbYpTmhEhcL3hwfYwjzAfDpqVdIeQJt9WzQjkV1F+Ke8ZPqDcfqIPwNrBl8DA44d+qEtSpX0OAuuxMeArCLjZWVOhZLcGtizrp0hzVbx7r869+nRPhZ+NzRy3iP3ZYX3gsxP6Zu05eE=',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
